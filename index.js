34539d26b4b9bf1fb5e9ddada52fa3c4b362ff22
NecroFatality <jsantimora@hotmail.com>
2025-09-02 18:30:54 -0700

    more switch changes

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const filesys = require("fs").promises;
 
 const listens = function( Requests, Responses) {
     console.log( Requests.url );
-    
+
     let returns = function(data){
         Responses.writeHead(200);
         Responses.end(data);
@@ -17,25 +17,22 @@ const listens = function( Requests, Responses) {
   
 
 
-    switch (Requests.url){
-        case "/":  
-        //check request url, if root, return html file
-        filesys.readFile(__dirname + "/home.html").then(
-            data => {  // same as function(data) {...} V
+    switch (true){
+        case Requests.url === "/":  
+      //return html file
+        filesys.readFile(__dirname + "/home.html")
+        .then(data => {  // same as function(data) {...} V
     
                     Responses.setHeader("Content-Type", "text/html; charset=UTF-8");
-                    returns(data);
+                    returns(data, "text/html");
             })
                     break;
 
-        case "/json": 
+        case Requests.url.startsWith("/") && Requests.url !== "/": 
+                    filesys.readFile(__dirname + "/data.json")
                     Responses.setHeader("Content-Type", "application/json; charset=UTF-8");
-                    returns(data);
+                    returns(data,"application/json");
                     break;
-
-                    default:
-                        Responses.writeHead(404, { "Content-Type": "text/plain; charset=UTF-8" });
-                        Responses.end("404 Not Found");
                 }
             };
     
